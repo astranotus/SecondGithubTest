a02913b680ad86a0f2b84b394881d6c2c4ca326a
astranotus <astranotus8@gmail.com>
2023-09-17 03:47:54 +0400

    created selectByHover test

---
+++ src/test/java/GithubTest.java
@@ -0,0 +1,27 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GithubTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    public void selectByHover(){
+        open("/");
+        $(".header-menu-wrapper").$(byTagAndText("button", "Solutions")).hover();
+        $(".header-menu-wrapper").$(byTagAndText("a", "Enterprise")).click();
+        $(withText("Build like the best")).shouldBe(visible);
+    }
+}
